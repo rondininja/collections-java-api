c37c98d2f6f887b8dc0a034f07f6d07b020de4f1
rondininja <phrondini@gmail.com>
2025-05-07 17:04:05 -0300

    pesquisas em set

---
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,61 @@
+package main.java.set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato c : contatoSet) {
+            if(c.getNome().startsWith(nome)) {
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for(Contato c : contatoSet) {
+            if(c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.adicionarContato("Pedro", 123456);
+        agendaContatos.adicionarContato("Pedro", 0);
+        agendaContatos.adicionarContato("Pedro Rondini", 111111);
+        agendaContatos.adicionarContato("Pedro Rondas", 654987);
+        agendaContatos.adicionarContato("Dona Guta", 111111);
+
+        agendaContatos.exibirContato();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Pedro"));
+
+        System.out.println("Contato atualizado: " + agendaContatos.atualizarNumeroContato("Dona Guta", 555555));
+    }
+}
